cb348ad2f780581855cd38c18bf0a2b6f2101d21
soljiiii <ggg454538@gmail.com>
2024-07-09 16:20:05 +0900

    프로젝트 생성

---
+++ src/App.jsx
@@ -0,0 +1,28 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div className='bodyContainer'>
+        <div className='helloContainer'>
+
+        </div>
+        <div className='introContainer'>
+
+        </div>
+        <div className='skillContainer'>
+
+        </div>
+        <div className='projectContainer'>
+
+        </div>
+        <div className='projectButtonContainer'>
+          
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default App
